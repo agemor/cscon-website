2609dcdbbf1d701cb36fefe8bb7766d0792590b8
HyunJun Kim <me@hyunjun.org>
2016-07-14 18:03:36 +0900

    Add committee introduction page

--- committee.php
+++ committee.php
@@ -2,9 +2,265 @@
   include "header.php";
 ?>
 
+<div class="well" style="margin-top:-20px; padding:0px">
+  <div class="container" style="padding-top: 20px; padding-bottom: 20px">
+    <h3>조직위원회 소개</h3>
+  </div>  
+</div>
+
+
 <div class="container">
 
-<h2>조직위원회 소개</h2>
+<div class="page-header">
+  <h4>조직위원회 소개</h4>
+</div>
+
+<p class="text-content">CSCON 조직위원회는 대학생 창업 네트워크 활성화의 필요성을 하나의 움직임으로 보여주고자 하는 대학생 활동가들로 구성되어 있습니다.</p>
+<p class="text-content">행사 프로그램 기획부터 연사 섭외, 행정 업무까지 컨퍼런스 준비의 모든 부분을 맡아 진행하고 있으며, 창의적이고 책임감 있는 의사결정을 통해 행사의 가치를 스스로 만들어 나가는 비영리 조직입니다.</p>
+
+
+<div class="page-header">
+  <h4>조직위원회 구성 <small>(가나다순 정렬)</small></h4>
+</div>
+
+
+    <div class="row">
+
+
+
+
+
+
+
+<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>안뇽하세요!</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>김다정 (DaJeong Kim)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>김현준 (HyunJun Kim)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 조직위원장
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>남지수 (Jisu Nam)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>남길현 (GilHyun Nam)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>박준혁 (JunHyuk Park)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>백운성 (UnSeung Baek)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>이민구 (Mingu Lee)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>정기호 (Kiho Jung)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>정지수 (Jisu Jeong)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 10px">
+        <div class="row">
+          <div class="col-xs-6 col-sm-12">
+            <div class="hovereffect text-center" style="margin-bottom: 0px">
+              <img class="img-responsive" src="./images/default-profile.png" alt="" style="width: 100%">
+              <div class="overlay text-center">
+                <h2>상태 메시지</h2>
+                <a class="info" href="#">프로필 보기</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-12" style="margin-bottom: 20px">
+            <div class="text-center">
+              <h4>황선영 (Sunyoung Hwang)</h4>
+              <h5 style="color:grey;"><a href="http://www.yonsei.ac.kr/" style="color:grey;">연세대학교</a> / 디렉터
+          </h5>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+     
+    </div> <a class="btn btn-default btn-sm pull-right" style="margin-top: -80px" href="#">
+      모든 기수 보기
+<span aria-hidden="true">&raquo;</span></a>
+
+
+    <div class="page-header">
+  <h4>조직위원회 지원</h4>
+</div>
+<p class="text-content">&nbsp;&nbsp;
+CSCON은 열정 넘치는 조직위원회 멤버를 언제나 기다리고 있습니다. 최고의 열정가들과 함께 행사를 처음부터 조직해 나가는 경험은 분명 새로운 삶의 인사이트를 줄 것입니다. 매년 1회 정기적으로 멤버를 모집하니, 자세한 사항은 공지사항을 참고하세요.<br><br>
+</p>
 
 </div>
 
---
+++ sponsors.php
@@ -0,0 +1,19 @@
+<?php
+  include "header.php";
+?>
+
+<div class="well" style="margin-top:-20px; padding:0px">
+  <div class="container" style="padding-top: 20px; padding-bottom: 20px">
+    <h3>후원 및 주관 기관</h3>
+  </div>  
+</div>
+
+
+<div class="container">
+
+
+</div>
+
+<?php
+  include "footer.php";
+?>
